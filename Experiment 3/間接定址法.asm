1f172a93a4c355c02ed48d6fed9fd4f359f50a7f
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2023-09-12 15:36:00 +0800

    Create 間接定址法.asm

---
+++ Experiment 3/間接定址法.asm
@@ -0,0 +1,8 @@
+ORG 0         ; code start from 0
+MOV R0, #30H  ; R0 = 30H
+MOV R1, #10H  ; R0 = 10H
+MOV 30H, #55H ; address 30H = 55H
+MOV 31H, #24H ; address 31H = 24H
+MOV A, @R0    ; A = ??? 55H
+SJMP $        ; infinite loop
+END
