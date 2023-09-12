19b6db852388efd07fd872c30c19e322906a323d
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2023-09-12 17:29:22 +0800

    Create code.c

---
+++ Experiment 12/code.c
@@ -0,0 +1,16 @@
+#include <regx51.h>
+void delay(unsigned intt);
+void main()
+{
+    while(1)
+    {
+        P2_0 = 0; //relay and LED are on
+        delay(50000);
+        P2_0 = 1; //relay and LED are off
+        delay(50000);
+    }
+}
+void delay(unsigned int t)
+{
+    while(t--);
+}
