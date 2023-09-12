bd6b5984396cb66fb68f9799d88b427eee21dc41
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2023-09-12 13:24:45 +0800

    Update 四合一七段顯示器.s

--- Experiment 1/四合一七段顯示器.s
+++ Experiment 1/四合一七段顯示器.s
@@ -6,19 +6,19 @@ START:  MOV R0, #4        ; 4 LED
 LOOP:   MOV P2, R2        ; select LED
         MOV A, R1         ; move R1 to A
         MOVC A, @A+DPTR   ; get value from table
-        MOV P1, A ; move value to P1
-        CALL DELAY ; delay 0.5 ms
-        INC R1 ; increase R1
-        MOV A, R2 ; move R2 to A
-        RL A ; rotate A
-        MOV R2, A ; move A back to R2
-        DJNZ R0, LOOP ; decrement R0 until 0
-        SJMP START ; jump to start
+        MOV P1, A         ; move value to P1
+        CALL DELAY        ; delay 0.5 ms
+        INC R1            ; increase R1
+        MOV A, R2         ; move R2 to A
+        RL A              ; rotate A
+        MOV R2, A         ; move A back to R2
+        DJNZ R0, LOOP     ; decrement R0 until 0
+        SJMP START        ; jump to start
 DELAY:  MOV R3, #250 ; loop 250 times
-        DJNZ R3, $ ; decrement R3 until 0
-        RET ; return from subroutine
-TABLE:  DB 0C0H ; 0
-        DB 0F9H ; 1
-        DB 0A4H ; 2
-        DB 0B0H ; 3
-        END ; end of program
+        DJNZ R3, $        ; decrement R3 until 0
+        RET               ; return from subroutine
+TABLE:  DB 0C0H           ; 0
+        DB 0F9H           ; 1
+        DB 0A4H           ; 2
+        DB 0B0H           ; 3
+        END               ; end of program
